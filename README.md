029a794a4826677bbfb47ebf421e6567b1f4ee14
Roman Storm <rstormsf@gmail.com>
2019-10-03 00:15:13 -0700

    add

---
+++ README.md
@@ -0,0 +1,17 @@
+## update gas price in postgres
+
+## install
+
+```
+npm i 
+cp .env.example .env
+```  
+
+setup `POSTGRES_STRING` 
+
+.env
+```bash
+POSTGRES_STRING=postgresql://USERNAME:PASSWORD@IP:PORT/DBNAME
+```
+
+node index.js
---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const fetch = require('node-fetch')
+const { Client } = require('pg');
+const { toWei } = require('web3-utils');
+
+const connectionString = process.env.POSTGRES_STRING
+
+const client = new Client({
+  connectionString: connectionString,
+})
+client.connect()
+
+
+
+let gasPrices = {
+  standard: 0,
+  fast: 0
+}
+const smartContractPollTime = 5  // in seconds
+const gasOracleUrls = [
+  'https://gasprice.poa.network/',
+  'https://www.etherchain.org/api/gasPriceOracle'
+]
+
+function fetchGasPrice() {
+  let oracleIndex = 0
+  const getPrices = async (resolve, reject) => {
+    try {
+      const response = await fetch(gasOracleUrls[oracleIndex % gasOracleUrls.length])
+      if (response.status === 200) {
+        const json = await response.json()
+
+        if (json.standard) {
+          gasPrices.standard = Number(json.standard)
+        }
+        if (json.fast) {
+          gasPrices.fast = Number(json.fast)
+        }
+      } else {
+        throw Error('Fetch gasPrice failed')
+      }
+
+      if (gasPrices.standard === 0 || gasPrices.fast === 0) {
+        throw Error('Invalid data returned')
+      }
+      resolve(gasPrices)
+    } catch (e) {
+      console.error(e)
+      oracleIndex++
+      setTimeout(() => getPrices(resolve, reject), 1000 * smartContractPollTime)
+    }
+  }
+  return new Promise(getPrices)
+
+}
+
+async function main() {
+  const prices = await fetchGasPrice();
+  const fast = toWei(prices.fast.toString(), 'gwei')
+  console.log('fast gas price is', prices.fast , 'gwei')
+  const querytext = 'UPDATE configurations SET value = $1 WHERE name = $2'
+  client.query(querytext, [fast, 'ETH_GAS_PRICE_DEFAULT'])
+    .then()
+    .catch(e => console.error(e.stack))
+    .then(() => client.end())
+}
+main()
